c72fe29b41fb9d3a878ea0cb5506f705231d3c60
kendrick90 <50811104+kendrick90@users.noreply.github.com>
2021-05-15 02:06:15 -0700

    Initial commit

---
+++ src/mini12864.h
@@ -0,0 +1,42 @@
+// #include <Arduino.h>
+// #include <SPI.h>
+// #include <U8g2lib.h>
+#include <Adafruit_NeoPixel.h>
+// #include <SD.h>
+// #include <ESP32Encoder.h>
+// #include <avdweb_Switch.h>
+// #include <Tone32.h>
+
+// #define SPI_CLK_PIN 18
+// #define SPI_MOSI_PIN 23
+// #define SPI_MISO_PIN 19
+
+// #define SPI_LCD_CS_PIN 5
+// #define SPI_SD_CS_PIN 4
+
+// #define ROTATION U8G2_R0
+// #define LCD_A0_DC_PIN 27
+// #define LCD_RESET_PIN 26
+// #define LCD_CONTRAST 255
+
+#define LED_PIN 25
+#define LED_COUNT 3
+#define BRIGHTNESS 100
+
+// #define BTN_EN1 33
+// #define BTN_EN2 34
+// #define BTN_ENC 25
+// #define EXTRA_BUTTON_PIN 35
+
+// #define BUZZER_PIN 32
+// #define BUZZER_CHANNEL 0
+
+void configure_leds(void);
+// void configure_lcd(void);
+// void configure_serial(void);
+// void configure_sd(void);
+// void configure_encoder(void);
+// void doEncoder(void);
+// void doPushButtons(void);
+void rainbowCycle(uint32_t LED_SLOWDOWN);
+// void testBuzzer(void);
